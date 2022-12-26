b16973b09080a2ac85768b05d874e81383a48472
haeun Noh <haeunNoh06>
2022-12-26 11:50:47 +0900

    [문법] switch 적용 후

--- Moonbub/switch.c
+++ Moonbub/switch.c
@@ -4,12 +4,15 @@ int main(void) {
 
 	int a = 1;
 
-	if (a == 1)
-		printf("apple");
-	else if (a == 2)
-		printf("banana");
-	else
+	//a값이 정수로 딱 하나만 떨어지는 경우 switch를 쓸 것
+	switch (a) {
+	case 1: 
+		printf("apple"); break;
+	case 2: 
+		printf("banana"); break;
+	default: 
 		printf("mango");
+	}
 
 	return 0;
 }
