e695049a344c6f8787fb20001bd49d1153573332
haeun Noh <okhagrace06@naver.com>
2022-12-30 13:30:00 +0900

    [AS2] 마우스 좌표 확인

--- AfterSchool2/main.cpp
+++ AfterSchool2/main.cpp
@@ -11,8 +11,14 @@ int main(void) {
 	RenderWindow window(VideoMode(1200, 800), "AfterSchool2");// 화면 크기, 제목
 	window.setFramerateLimit(60);// 60초마다 한 번씩 돌아간다
 
+	// f : 소수
+	// Vector2i : 정수
+	Vector2i mouse_pos;// 마우스 좌표
+
 	while (window.isOpen())
 	{
+		mouse_pos = Mouse::getPosition(window);// 마우스 좌표 실시간으로 받기
+
 		Event event;
 		while (window.pollEvent(event))
 		{
@@ -23,6 +29,8 @@ int main(void) {
 				break;
 			}
 		}
+
+		printf("%d, %d", mouse_pos.x, mouse_pos.y);// 마우스 x값 y값 출력
 	}
 
 	return 0;
