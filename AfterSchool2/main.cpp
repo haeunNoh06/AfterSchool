eaa41cbb880de85251ae324747af3af05cdaf75b
haeun Noh <okhagrace06@naver.com>
2022-12-30 13:23:35 +0900

    [AS2] 기본틀 완성

---
+++ AfterSchool2/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace sf;
+
+int main(void) {
+
+	RenderWindow window(VideoMode(1200, 800), "AfterSchool2");// 화면 크기, 제목
+	window.setFramerateLimit(60);// 60초마다 한 번씩 돌아간다
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case Event::Closed:
+				window.close();
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
