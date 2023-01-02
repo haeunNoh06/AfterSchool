4a3d7d8599ae87d3d05d7d6b0f55bc001b19d4cb
haeun Noh <okhagrace06@naver.com>
2023-01-02 13:42:33 +0900

    [문법] 2차원 배열 shuffle

--- Moonbub/shuffle.c
+++ Moonbub/shuffle.c
@@ -10,24 +10,29 @@ void swap(int* a, int* b)
 }
 
 // arr의 내용물을 무작위로 섞는다
-void shuffle(int* arr)
+void shuffle(int* arr[][4])
 {
 	srand(time(0));
-	for (int i = 0; i < 100; i++) {
-		// 0~15까지 나와야 하므로 16의 나머지를 구하는 연산을 사용
-		swap(&arr[rand() % 16], &arr[rand() % 16]);
+	for (int i = 0; i < 25; i++) {
+		for (int j = 0; j < 25; j++) {
+			// 0~15까지 나와야 하므로 16의 나머지를 구하는 연산을 사용
+			swap(&arr[rand()%4][rand() % 4], &arr[rand() % 4][rand()%4]);
+		}
 	}
 }
 
 int main(void) {
 
 	const int S = 4;
-	int arr[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
+	//int arr[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
+	int arr[4][4] = { 100,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
 
 	shuffle(arr);
 
-	for (int i = 0; i < 16; i++) {
-		printf("%d ", arr[i]);
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf("%d ", arr[i][j]);
+		}
 	}
 
 	return 0;
