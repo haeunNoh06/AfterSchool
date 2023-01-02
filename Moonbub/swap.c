b1bf37eb3ab9562a8aaa2678bb5b888fa1d6c01a
haeun Noh <okhagrace06@naver.com>
2023-01-02 11:41:50 +0900

    [문법] 포인터를 이용하여 swap함수 호출

---
+++ Moonbub/swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int* a, int* b)
+{
+	int swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
+int main(void) {
+
+	int a = 3;
+	int b = 5;
+	printf("a: %d, b: %d\n", a, b);
+
+	// a와 b를 swap하는 함수를 호출
+	swap(&a, &b);
+	printf("a: %d, b: %d\n", a, b);
+
+	return 0;
+}
