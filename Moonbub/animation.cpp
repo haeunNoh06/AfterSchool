567d60606f1407be0992ac213cd752060508d8d9
haeun Noh <okhagrace06@naver.com>
2023-01-03 15:21:48 +0900

    [기능] 방향에 따라 좌우로 움직임

--- Moonbub/animation.cpp
+++ Moonbub/animation.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
@@ -7,12 +8,20 @@ struct Player {
 	RectangleShape sprite;
 	int fps;// 초당 프레임 갯수 (frame per sec)
 	int idx;// 애니메이션 인덱스
+	int frames;// 애니메이션 frame 수
+	long ani_time;// 애니메이션이 바뀔 때의 시각
+	long ani_delay;// 화면 전환의 텀
+	int speed;// 속도
+
 };
 
 int main(void)
 {
 	RenderWindow window(VideoMode(1200, 800), "Animation");
 	window.setFramerateLimit(60);
+
+	long start_time;
+	long spent_time;
 
 	Texture run[10];
 	run[0].loadFromFile("./resources/images/Run__000.png");
@@ -31,10 +40,17 @@ int main(void)
 	player.fps = 10;
 	player.sprite.setTexture(&run[0]);
 	player.sprite.setSize(Vector2f(90, 120));
-	player.sprite.setPosition(200, 200);
+	player.sprite.setPosition(200, 400);
+	player.frames = 10;
+	player.ani_delay = 1000 / player.frames / 2;// 0.5초마다 걸음
+	player.speed = 5;
+
+	start_time = clock();
+	player.ani_time = start_time;
 
 	while (window.isOpen())
 	{
+		spent_time = clock() - start_time;
 		Event event;
 		while (window.pollEvent(event))
 		{
@@ -48,6 +64,26 @@ int main(void)
 			}
 		}
 
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			player.sprite.setScale(1, 1);
+			player.sprite.move(player.speed, 0);
+
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			player.sprite.setScale(-1, 1);
+			player.sprite.move(-player.speed, 0);
+		}
+
+		// 0.1초마다 애니메이션 그림이 바뀜
+		while (spent_time - player.ani_time > 1000/player.ani_delay)
+		{
+			// 반복하려고
+			player.ani_time = spent_time;
+			player.sprite.setTexture(&run[player.idx%player.frames]);
+			player.idx++;
+		}
 
 		window.clear(Color::Magenta);
 
