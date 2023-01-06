425cec61e04c1e4c74c5c580d2defaf872afe2e5
haeun Noh <okhagrace06@naver.com>
2023-01-06 14:00:21 +0900

    UPDATE

--- mysql_ex/main.c
+++ mysql_ex/main.c
@@ -21,18 +21,30 @@ int main(void)
         printf("DB 연결 성공\n");
 
     int query_result;
-    char insert_q[50] = "INSERT INTO korea VALUES('TOKO', 10000)";
+    //char insert_q[50] = "INSERT INTO korea VALUES('TOKO', 10000)";
+    //
+
+    //// Query 실행
+    //query_result = mysql_query(connection, insert_q);
+   
+    //// 성공하면 0, 실패하면 다른 값을 반환 (ex 100)
+    //if (query_result == 0)
+    //    printf("%s 성공\n", insert_q);
+    //else
+
+    // UPDATE
+    char update_q[100] = "UPDATE korea SET population = 100000 WHERE city = 'TOKO'";
     
     // Query 실행
-    query_result = mysql_query(connection, insert_q);
+    query_result = mysql_query(connection, update_q);
    
     // 성공하면 0, 실패하면 다른 값을 반환 (ex 100)
     if (query_result == 0)
-        printf("%s 성공\n", insert_q);
+        printf("%s 성공\n", update_q);
     else
-        printf("%s 실패\n", insert_q);
+        printf("%s 실패\n", update_q);
 
-    
+
     mysql_close(&mysql);
 }
 
